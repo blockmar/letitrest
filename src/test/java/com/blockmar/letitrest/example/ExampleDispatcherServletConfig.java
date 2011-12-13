f3d2fb7a8a4e5d9717bfe4deee04c3721673ded1
Anders Blockmar <git@blockmar.com>
2011-12-13 18:16:27 +0100

    Added Override annotation to Example config

--- src/test/java/com/blockmar/letitrest/example/ExampleDispatcherServletConfig.java
+++ src/test/java/com/blockmar/letitrest/example/ExampleDispatcherServletConfig.java
@@ -7,6 +7,7 @@ import com.blockmar.letitrest.servlet.DispatcherServletConfig;
 
 public class ExampleDispatcherServletConfig extends DispatcherServletConfig {
 
+	@Override
 	public Set<Object> getControllers() {
 		Set<Object> contrllers = new HashSet<Object>();
 		contrllers.add(new ExampleController());
